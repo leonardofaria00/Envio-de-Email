2b74aaabf0e10c2a44a97ccd8020cb9aeb8ba2df
Leonardo Faria <leonardofaria00@gmail.com>
2019-01-29 10:37:49 -0200

    Update index.php

--- enviaEmail/index.php
+++ enviaEmail/index.php
@@ -15,8 +15,8 @@ $mail->From = "correio@correio.com.br";
 $mail->FromName = "Nome da Empresa";
 $mail->CharSet = "UTF-8";
 
-$emailUsuario = 'seuEmail@seuEmail.com';
-$nomeUsuario = 'Fulano de tal';
+$emailUsuario = 'seuEmail@seuEmail.com.br';
+$nomeUsuario = 'Fulano';
 
 $mail->AddAddress($emailUsuario, $nomeUsuario);
 $mail->WordWrap = 50; //wrap seta o tamanhdo do texto por linha
